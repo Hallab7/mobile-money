463348976dab6d57fd25f200a6aade4b293f7bb0
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-28 16:39:35 +0100

    add: impl TokenService.storeRefreshToken

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import crypto from "node:crypto";
+import { pool } from "../config/database";
 import { env } from "../config/env";
+import { redisClient } from "../config/redis";
 
 class TokenService {
   // Generate refresh token with JWT ID (JTI)
@@ -32,6 +34,46 @@ class TokenService {
       expiresIn,
     };
   }
+
+  // Store refresh token in db
+  async storeRefreshToken(
+    userId: string,
+    token: string,
+    jti: string,
+    deviceName: string,
+    ipAddress: string,
+    userAgent: string,
+  ) {
+    const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+    const expiresIn = parseInt(env.REFRESH_TOKEN_EXPIRES_IN);
+    const expiresAt = new Date(Date.now() + expiresIn);
+
+    try {
+      const result = await pool.query(
+        `INSERT INTO refresh_tokens 
+            (user_id, token_jti, device_name, ip_address, user_agent, expires_at)
+            VALUES ($1,$2,$3,$4,$5,$6,$7)
+            RETURNING id, token_jti, device_name, created_at, expires_at`,
+        [userId, jti, tokenHash, deviceName, ipAddress, userAgent, expiresAt],
+      );
+
+      // Store in Redis for quicker lookups (with TTL)
+      await redisClient.setEx(
+        this.label(jti),
+        expiresIn,
+        JSON.stringify({ userId, active: true }),
+      );
+
+      return result.rows;
+    } catch (err) {
+      console.error("Error storing refresh token:", err);
+      throw err;
+    }
+  }
+
+  private label(lbl: string) {
+    return `refresh_token:${lbl}`;
+  }
 }
 
-export const tokenService = new TokenService()
+export const tokenService = new TokenService();
